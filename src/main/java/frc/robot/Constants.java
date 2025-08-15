1a561ef683bd2bd886b6fe8f9a4826b7a97a5063
Thomas Ahn <166737116+quakeq@users.noreply.github.com>
2025-08-14 20:20:40 -0700

    Subsystem classes+constant creation

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,7 +4,6 @@
 
 package frc.robot;
 
-
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -15,11 +14,67 @@ package frc.robot;
  */
 public final class Constants {
   public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
+
   }
 
+  //====================Intake (3_)====================
+  public static class IntakeConstants {
 
+    public static final int intakePivotPort = 30;
+    public static final int intakeRollerPort = 31;
+    public static final int intakeL1StopperPort = 32;
+
+  }
+
+  //====================Elevator (4_)====================
+  public static class ElevatorConstants {
+
+    public static final int elevatorRightPort = 40;
+    public static final int elevatorLeftPort = 41;
+    public static final int elevatorExtraPort = 42;
+
+      
+  }
+
+  //====================End Effector (5_)====================
+  public static class EndEffectorConstants {
+    public static final int endEffectorPivotPort = 50;
+    public static final int endEffectorRollerPort = 51; 
+  }
+
+  //====================Climb (6_)====================
   public static class ClimbConstants {
+    public static final int climbPort = 60;
+    
   }
 
+  //====================Indexer (7_)====================
+  public static class IndexerConstants {
+    public static final int indexerRightPort = 70;
+    public static final int indexerLeftPort = 71;
+    public static final int indexerCANrange = 72;
+
+  }
+  
+  
+
+
+
+  public record PIDgains(double kP, double kI, double kD, double kS, double kV, double kA, double kG) {
+
+        //Pure PID (the average use case)
+        public PIDgains(double kP, double kI, double kD) {
+            this(kP, kI, kD, 0, 0, 0,0);
+        }
+
+        //Arm/Elevator PID (we don't care about voltage or acceleration)
+        public PIDgains(double kP, double kI, double kD, double kS, double kG){
+            this(kP, kI, kD, kS, 0, 0, kG);
+        }
+
+        //Flywheels/Wheels (since kG is not an issue)
+        public PIDgains(double kP, double kI, double kD, double kS, double kV, double kA){
+            this(kP, kI, kD, kS, kV, kA, 0);
+        }
+    }
 }
