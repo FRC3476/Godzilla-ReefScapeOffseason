c640309a3538d541409a68d3a081d6b9ebab05ab
John Fogarty <john.fogarty@aol.com>
2025-08-23 19:48:30 -0400

    Fix Folder, Structure

--- src/main/java/frc/robot/subsystems/climb/Climber.java
+++ src/main/java/frc/robot/subsystems/climb/Climber.java
@@ -1,5 +1,5 @@
 
-package frc.robot.subsystems.Climber;
+package frc.robot.subsystems.climb;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
--- src/main/java/frc/robot/subsystems/climb/ClimberIO.java
+++ src/main/java/frc/robot/subsystems/climb/ClimberIO.java
@@ -1,4 +1,4 @@
-package frc.robot.subsystems.Climber;
+package frc.robot.subsystems.climb;
 
 import org.littletonrobotics.junction.AutoLog;
 
