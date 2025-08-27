23388d1eebd1299aae033ec8a4eaa9c6f89c679d
armyoffrog <sidneyfroge@gmail.com>
2025-08-26 21:28:29 -0700

    created check for coral in ground intake

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,12 +1,21 @@
 package frc.robot.subsystems.intake;
 
+import com.ctre.phoenix6.hardware.CANrange;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
   private final IntakeIO io;
   private final IntakeIOInputsAutoLogged inputs = new IntakeIOInputsAutoLogged();
 
+  private final CANrange canRange;
+
   public Intake(IntakeIO io) {
     this.io = io;
   }
+
+  public boolean isCoral() {
+    return canRange.getIsDetected().getValue();
+  }
+
 }
