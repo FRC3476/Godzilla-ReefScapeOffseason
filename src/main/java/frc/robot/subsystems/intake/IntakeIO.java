11f239ef0d274a8eb1af696bce75b6908b6645cd
Raptorly1 <prahas.reddy08@gmail.com>
2025-08-30 16:45:24 -0700

    Added volt

--- src/main/java/frc/robot/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -50,4 +50,7 @@ public interface IntakeIO {
       boolean isSensorConnected, boolean tripped, double signalStrength, double distanceMeters) {}
 
   default void updateInputs(IntakeIOInputs inputs) {}
+  default void setPivotVoltage(double voltage) {}
+  default void setRollerVoltage(double voltage) {}
+  default void setLvl1BlockerVoltage(double voltage) {}
 }
