7eea062df1b01ec60b454c793cca173e3e61c96d
conlan-g <conlan.goodemote@gmail.com>
2025-09-04 19:24:11 -0700

    create basic subsystem, implement detect coral in feeder and roller voltage control

---
+++ src/main/java/frc/robot/subsystems/feeder/Feeder.java
@@ -0,0 +1,27 @@
+package frc.robot.subsystems.feeder;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
+
+public class Feeder extends SubsystemBase {
+  private final FeederIO io;
+  private final FeederIOInputsAutoLogged inputs = new FeederIOInputsAutoLogged();
+
+  public Feeder(FeederIO io) {
+    this.io = io;
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Feeder", inputs);
+  }
+
+  public boolean isCoralInFeeder() {
+    return inputs.canRangeData.tripped();
+  }
+
+  public void setRollerVoltage(double voltage) {
+    io.setRollerVoltage(voltage);
+  }
+}
