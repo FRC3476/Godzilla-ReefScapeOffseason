943db99ab006920386f7df24a2667b30a32be158
Shriya-vis <shriya.visu@gmail.com>
2025-09-06 14:51:32 -0700

    Added functions stop and deploy

--- src/main/java/frc/robot/subsystems/climb/Climber.java
+++ src/main/java/frc/robot/subsystems/climb/Climber.java
@@ -1,5 +1,18 @@
 package frc.robot.subsystems.climb;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Climber extends SubsystemBase {}
+public class Climber extends SubsystemBase {
+
+    public Command climbDeploy (){
+        return Commands.run(() -> this.io.runVolts(ClimberIntakeVolts.get()), this);
+    }
+
+    public Command intakeSTOP() {
+    return Commands.run(() -> this.io.runVoltage(0), this);
+  }
+}
+
+
