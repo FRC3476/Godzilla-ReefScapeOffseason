751c839abda1a1e8f425e3c5fc014976208d3dd9
conlan-g <conlan.goodemote@gmail.com>
2025-09-06 15:50:06 -0700

    forgot static

--- src/main/java/frc/robot/util/MotorStallDetection.java
+++ src/main/java/frc/robot/util/MotorStallDetection.java
@@ -3,7 +3,7 @@ package frc.robot.util;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 public class MotorStallDetection {
-    public boolean isMotorStalled(TalonFX motor, double currentLimitAmps, double velocityLimitRPS) {
+    public static boolean isMotorStalled(TalonFX motor, double currentLimitAmps, double velocityLimitRPS) {
         return motor.getStatorCurrent().getValueAsDouble() > currentLimitAmps && motor.getVelocity().getValueAsDouble() < velocityLimitRPS;
     }
 }
