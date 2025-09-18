29c44eb46065cdd428bf2b203950cceec0987e43
Shriya-vis <shriya.visu@gmail.com>
2025-09-17 20:11:59 -0700

    adding the voltage controls for pivot in EndEffectorIOReal

--- src/main/java/frc/robot/subsystems/end_effector/EndEffectorIOReal.java
+++ src/main/java/frc/robot/subsystems/end_effector/EndEffectorIOReal.java
@@ -49,8 +49,10 @@ public class EndEffectorIOReal implements EndEffectorIO {
     rollerTalonFX = new TalonFX(EndEffectorConstants.rollerID);
     coralCANRange = new CANrange(EndEffectorConstants.coralCANRangeID);
 
-    PhoenixUtil.tryUntilOk(5, () -> pivotTalonFX.getConfigurator().apply(EndEffectorConstants.PIVOT_TALON_CONFIG));
-    PhoenixUtil.tryUntilOk(5, () -> rollerTalonFX.getConfigurator().apply(EndEffectorConstants.ROLLER_TALON_CONFIG));
+    PhoenixUtil.tryUntilOk(
+        5, () -> pivotTalonFX.getConfigurator().apply(EndEffectorConstants.PIVOT_TALON_CONFIG));
+    PhoenixUtil.tryUntilOk(
+        5, () -> rollerTalonFX.getConfigurator().apply(EndEffectorConstants.ROLLER_TALON_CONFIG));
 
     pivotPosition = pivotTalonFX.getPosition();
     pivotAppliedVolts = pivotTalonFX.getMotorVoltage();
@@ -128,5 +130,9 @@ public class EndEffectorIOReal implements EndEffectorIO {
 
   public void setRollerVoltage(double voltage) {
     rollerTalonFX.setControl(roller_m_request.withOutput(voltage));
+  }
+
+  public void setPivotVoltage(double voltage) {
+    rollerTalonFX.setControl(pivot_m_request.withPosition(voltage));
   }
 }
