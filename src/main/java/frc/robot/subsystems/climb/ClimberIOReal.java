d0f74d9876b0ab0674683d09426162f611378548
Shriya-vis <shriya.visu@gmail.com>
2025-09-20 16:37:05 -0700

    climber position controls

--- src/main/java/frc/robot/subsystems/climb/ClimberIOReal.java
+++ src/main/java/frc/robot/subsystems/climb/ClimberIOReal.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.climb;
 
 import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.controls.*;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.units.measure.Angle;
@@ -27,6 +28,8 @@ public class ClimberIOReal implements ClimberIO {
 
   // control requests
   private final VoltageOut voltsRequest = new VoltageOut(0.0).withUpdateFreqHz(0.0);
+
+  private MotionMagicVoltage climb_m_request = new MotionMagicVoltage(0).withEnableFOC(true);
 
   public ClimberIOReal() {
     talon = new TalonFX(Constants.ClimbConstants.ID);
@@ -61,5 +64,9 @@ public class ClimberIOReal implements ClimberIO {
   @Override
   public void runVolts(double volts) {
     talon.setControl(voltsRequest.withOutput(volts));
+  }
+
+  public void setClimbPosition(double position) {
+    talon.setControl(climb_m_request.withPosition(position));
   }
 }
