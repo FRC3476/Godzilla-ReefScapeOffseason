ab858f572be1fa8e50f5ff961be3eccc97134dcd
Shriya-vis <shriya.visu@gmail.com>
2025-09-26 19:48:19 -0700

    added back in motor stall detection and renamed stallamps constant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -423,8 +423,7 @@ public final class Constants {
     public static final double kP = 0.0;
     public static final double kI = 0.0;
     public static final double kD = 0.0;
-    public static final double PRE_CLIMB_POSITION = 0.0;
-    public static final double StallLimitAmps = 0.0;
+    public static final double STALL_AMPS = 0.0;
     public static final double STALL_VELOCITY = 0.0;
     public static final double CLIMB_DEPLOY_POSITION = 0.0;
     public static final double CLIMB_CLIMB_POSITION = 0.0;
