d6d55fa8f54af532d1f562f3184e2c2aae075388
conlan-g <conlan.goodemote@gmail.com>
2025-10-07 20:11:08 -0700

    format

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -560,8 +560,12 @@ public final class Constants {
     public static final double ROLLER_HOLDING_CORAL_VOLTS = 2;
 
     // Reef Collision Avoidance
-    public static final double FULLY_EXTENDED_DISTANCE_METERS = Units.inchesToMeters(24); // distance from the center of the robot to the end of the end effector
-    public static final double MIN_STOW_CLEARANCE_METERS = Units.inchesToMeters(24); // area around that point that would hit something on the end effector
+    public static final double FULLY_EXTENDED_DISTANCE_METERS =
+        Units.inchesToMeters(
+            24); // distance from the center of the robot to the end of the end effector
+    public static final double MIN_STOW_CLEARANCE_METERS =
+        Units.inchesToMeters(
+            24); // area around that point that would hit something on the end effector
   }
 
   // ====================Climb (6_)====================
--- src/main/java/frc/robot/RobotState.java
+++ src/main/java/frc/robot/RobotState.java
@@ -157,7 +157,8 @@ public class RobotState extends MagicVirtualSubsystem {
             .minus(FieldUtils.getClosestReef().rightPole.getPose())
             .getTranslation()
             .getNorm();
-    return distanceToLeft > EndEffectorConstants.MIN_STOW_CLEARANCE_METERS && distanceToRight > EndEffectorConstants.MIN_STOW_CLEARANCE_METERS;
+    return distanceToLeft > EndEffectorConstants.MIN_STOW_CLEARANCE_METERS
+        && distanceToRight > EndEffectorConstants.MIN_STOW_CLEARANCE_METERS;
   }
 
   public static final double LOOKBACK_TIME = 1.0;
