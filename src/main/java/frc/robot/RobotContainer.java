dd4ea21f8e2ec55394dcf9d8acf2d79223fe0c69
Code Orange FRC 3476 <pit@teamcodeorange.com>
2025-10-08 22:23:37 -0700

    commenting out barge auto transition triggers for now

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1726,14 +1726,14 @@ public class RobotContainer {
     feeder.dejamTrigger.onTrue(intake.dejamFeeder());
     elevator.elevatorObjectTrigger.onTrue(elevator.dejamElevator());
     intake.rejectCoralTrigger().whileTrue(intake.rejectCoralCommand());
-    RobotState.finishedBargeScoringForward()
-        .onTrue(
-            superstructure.setStateCommand(
-                SuperstructureState.BARGE_AIM_CENTER, "Auto set BARGE_AIM_CENTER after scoring"));
-    RobotState.finishedBargeScoringBackward()
-        .onTrue(
-            superstructure.setStateCommand(
-                SuperstructureState.STOW, "Auto set BARGE_AIM_CENTER after scoring"));
+    // RobotState.finishedBargeScoringForward()
+    //     .onTrue(
+    //         superstructure.setStateCommand(
+    //             SuperstructureState.BARGE_AIM_CENTER, "Auto set BARGE_AIM_CENTER after scoring"));
+    // RobotState.finishedBargeScoringBackward()
+    //     .onTrue(
+    //         superstructure.setStateCommand(
+    //             SuperstructureState.STOW, "Auto set BARGE_AIM_CENTER after scoring"));
   }
 
   private void configureSuperstructureTrigger() {
