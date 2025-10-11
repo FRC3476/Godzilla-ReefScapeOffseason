deb74db2353a62424cc92335cc4d72b53edfb6e0
Jacob Quion <142964571+JacobQuion@users.noreply.github.com>
2025-10-10 20:24:48 -0700

    Added pathplanner command groups for test auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,6 +13,7 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.networktables.NetworkTable;
@@ -21,6 +22,7 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -35,6 +37,7 @@ import frc.robot.RobotState.ScoreLevel;
 import frc.robot.RobotState.ScorePosition;
 import frc.robot.commands.DriveCommands;
 import frc.robot.commands.DriveToPosePIDCommand;
+import frc.robot.commands.Score;
 import frc.robot.commands.test.CleaningTest;
 import frc.robot.subsystems.climb.Climber;
 import frc.robot.subsystems.climb.ClimberIO;
@@ -175,6 +178,52 @@ public class RobotContainer {
         drive = new DriveSubsystem(new DriveIO() {}, robotState);
         break;
     }
+
+    // ====================LOADING COMMANDS====================
+    NamedCommands.registerCommand("SuperStructureStartup", superstructure
+                                .setStateCommand(SuperstructureState.STOW, "STOW")
+                                .asProxy());
+
+    NamedCommands.registerCommand("IntakeStartup", intake.setIntakeStateCommand(IntakeState.IDLE).asProxy());
+
+    NamedCommands.registerCommand("ScoreTargetStartup",
+                                new InstantCommand(() -> robotState.getStoredScorePosition().setScoreLevel(ScoreLevel.L4)));
+
+    // ====================SCORING COMMANDS====================
+    NamedCommands.registerCommand("AimL4", superstructure.setStateCommand(SuperstructureState.L4_AIM, "L4 AIM").asProxy());
+
+    // LEFT ALIGN
+    NamedCommands.registerCommand("FinalLeftPoleAlign", new DriveToPosePIDCommand(drive, () ->
+                    PoseUtils.getPerpendicularOffsetPose(
+                    FieldUtils.getClosestReef().leftPole.getPose(),
+                    DriveConstants.AUTO_ALIGN_PERPENDICULAR_OFFSET))
+                    .withTimeout(1.0));
+
+    // RIGHT ALIGN
+    NamedCommands.registerCommand("FinalRightPoleAlign", new DriveToPosePIDCommand(drive, () ->
+                    PoseUtils.getPerpendicularOffsetPose(
+                    FieldUtils.getClosestReef().rightPole.getPose(),
+                    DriveConstants.AUTO_ALIGN_PERPENDICULAR_OFFSET))
+                    .withTimeout(1.0));
+    
+    NamedCommands.registerCommand("ConfirmScore", new Score(superstructure, claw, robotState));
+
+    NamedCommands.registerCommand("StowRobotState",
+                                    new WaitUntilCommand(() -> RobotState.isSafeToStow())
+                                    .andThen(superstructure.setStateCommand(SuperstructureState.STOW, "STOW").asProxy()));
+
+    // ====================INTAKE COMMANDS====================
+    NamedCommands.registerCommand("IntakeEnable", intake.setIntakeStateCommand(IntakeState.INTAKE).asProxy());
+
+    NamedCommands.registerCommand("DriveToCoral", DriveCommands.driveToCoral(drive, vision).withTimeout(3.0));
+
+    // ====================CORAL TRACKING COMMANDS====================
+    NamedCommands.registerCommand("SeesCoral", new WaitUntilCommand(() -> vision.isCoralDetected()).withTimeout(3.0));
+    NamedCommands.registerCommand("IsCoralInFeeder", new WaitUntilCommand(() -> CoralStateTracker.getCurrentPosition() == CoralPosition.AT_FEEDER));
+    NamedCommands.registerCommand("IsCoralInEndEffector", new WaitUntilCommand(
+                                        () -> CoralStateTracker.getCurrentPosition() == CoralPosition.STAGED_IN_END_EFFECTOR));
+
+    //============================================================================================================================================
 
     // Set up auto routines
     autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
---
+++ src/main/java/frc/robot/commands/Score.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.robot.Constants.EndEffectorConstants.ClawState;
+import frc.robot.RobotState;
+import frc.robot.subsystems.end_effector.Claw;
+import frc.robot.subsystems.superstructure.CoralStateTracker;
+import frc.robot.subsystems.superstructure.CoralStateTracker.CoralPosition;
+import frc.robot.subsystems.superstructure.Superstructure;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class Score extends SequentialCommandGroup {
+  /** Creates a new score. */
+  public Score(Superstructure superstructure, Claw claw, RobotState robotState) {
+    // Add your commands in the addCommands() call, e.g.
+    addCommands(
+        claw.setClawStateCommand(ClawState.SCORING).asProxy(),
+        new WaitUntilCommand(() -> CoralStateTracker.getCurrentPosition() == CoralPosition.NONE)
+            .withTimeout(3),
+        superstructure.setStateCommand(() -> robotState.getFadeawayState(), "Aim fade").asProxy(),
+        claw.setClawStateCommand(ClawState.IDLE).asProxy());
+  }
+}
