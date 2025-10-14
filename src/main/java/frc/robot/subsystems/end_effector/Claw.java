977198bb7748622d2554da9c82de68fa71e6701a
CodeOrange <pit@teamcodeorange.com>
2025-10-14 00:31:52 -0700

    add trigger to make coral state none as soon as we score. currently unused

--- src/main/java/frc/robot/subsystems/end_effector/Claw.java
+++ src/main/java/frc/robot/subsystems/end_effector/Claw.java
@@ -3,10 +3,12 @@ package frc.robot.subsystems.end_effector;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.EndEffectorConstants;
 import frc.robot.Constants.EndEffectorConstants.ClawState;
 import frc.robot.RobotState;
 import frc.robot.subsystems.superstructure.CoralStateTracker;
+import frc.robot.subsystems.superstructure.CoralStateTracker.CoralPosition;
 import frc.robot.util.LoggedTunableNumber;
 import frc.robot.util.RobotTime;
 import org.littletonrobotics.junction.Logger;
@@ -32,6 +34,8 @@ public class Claw extends SubsystemBase {
           "Claw/RollerScoringL1Volts", EndEffectorConstants.ROLLER_SCORING_L1_VOLTS);
 
   private ClawState currentState = ClawState.NONE;
+  private boolean firstSensorTriggered;
+  private boolean secondSensorTriggered;
 
   public Claw(ClawIO io) {
     this.io = io;
@@ -43,9 +47,9 @@ public class Claw extends SubsystemBase {
     Logger.processInputs("Claw", inputs);
     Logger.recordOutput("Claw/CurrentState", currentState);
 
-    boolean firstSensorTriggered =
+    firstSensorTriggered =
         inputs.firstCANRangeData.rangeIsTripped() && inputs.firstCANRangeData.canRangeConnected();
-    boolean secondSensorTriggered =
+    secondSensorTriggered =
         inputs.secondCANRangeData.rangeIsTripped() && inputs.secondCANRangeData.canRangeConnected();
 
     CoralStateTracker.updateFirstEndEffector(firstSensorTriggered);
@@ -64,12 +68,20 @@ public class Claw extends SubsystemBase {
     io.setRollerVoltage(voltage);
   }
 
-  public boolean isCoralInEndeffector() {
+  public boolean isClawScoring() {
+    return currentState == ClawState.SCORING || currentState == ClawState.SCORING_L1;
+  }
+
+  public boolean isCoralInClaw() {
     // Use CoralStateTracker instead of individual sensor readings
     CoralStateTracker.CoralPosition position = CoralStateTracker.getCurrentPosition();
     return position == CoralStateTracker.CoralPosition.AT_FIRST_END_EFFECTOR
         || position == CoralStateTracker.CoralPosition.AT_SECOND_END_EFFECTOR
         || position == CoralStateTracker.CoralPosition.STAGED_IN_END_EFFECTOR;
+  }
+
+  public Trigger exhaustedCoral() {
+    return new Trigger(() -> isClawScoring() && isCoralInClaw() && !firstSensorTriggered && !secondSensorTriggered);
   }
 
   public boolean isCoralAtFirstSensor() {
@@ -83,7 +95,7 @@ public class Claw extends SubsystemBase {
   }
 
   public boolean hasAlgae() {
-    return io.checkRollerStalled() && !isCoralInEndeffector();
+    return io.checkRollerStalled() && !isCoralInClaw();
   }
 
   public Command clawDefault() {
