b631238948ac876c1244beb74d5cab55652ca97e
Karl Olson <olsonkarlp@gmail.com>
2025-10-16 10:09:36 -0700

    missing AngularVelocity import

--- src/main/java/frc/robot/subsystems/climb/ClimbRollerIOReal.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbRollerIOReal.java
@@ -10,6 +10,7 @@ import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.ParentDevice;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.Temperature;
 import edu.wpi.first.units.measure.Voltage;
