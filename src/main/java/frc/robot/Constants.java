97bc250551edb740ff6652ebcfeb7267eb841827
Jacob Quion <142964571+JacobQuion@users.noreply.github.com>
2025-10-19 14:55:37 -0700

    Adjusted roller voltages

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -667,11 +667,11 @@ public final class Constants {
     }
 
     // Roller Voltages
-    public static final double ROLLER_INTAKE_CORAL_VOLTS = 10;
+    public static final double ROLLER_INTAKE_CORAL_VOLTS = 7;
     public static final double ROLLER_SCORING_VOLTS = -12;
     public static final double ROLLER_SCORING_L1_VOLTS = 6;
     public static final double ROLLER_SCORING_ALGAE_VOLTS = -8;
-    public static final double ROLLER_HOLDING_CORAL_VOLTS = 1;
+    public static final double ROLLER_HOLDING_CORAL_VOLTS = 3;
 
     // Reef Collision Avoidance
     public static final double FULLY_EXTENDED_DISTANCE_METERS =
