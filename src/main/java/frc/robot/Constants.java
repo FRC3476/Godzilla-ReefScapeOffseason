b362a785c922cf52b7e0cbd2a6be988c9c023e0b
CodeOrange <pit@teamcodeorange.com>
2025-11-01 13:38:26 -0700

    changed ee measurement to avoid hitting algae in reef

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -700,7 +700,7 @@ public final class Constants {
     // Reef Collision Avoidance
     public static final double FULLY_EXTENDED_DISTANCE_METERS =
         Units.inchesToMeters(
-            32); // distance from the center of the robot to the end of the end effector
+            36); // distance from the center of the robot to the end of the end effector
     public static final double END_EFFECTOR_POLES_CLEARANCE_METERS = Units.inchesToMeters(6);
   }
 
